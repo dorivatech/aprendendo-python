d426d8283cf72d77fad31a40bfe85d88ba8095d2
DorivaTech <dorivatech@gmail.com>
2021-06-06 17:00:41 +0100

    Hello Worldgit add .!

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+# Hello World
+print("Hello World")
+print()
+
+# Hello @FirstName
+first_name = input("Please insert your first name 🥺")
+print("Your first name is: " + first_name.upper())
+
+# Hello @LastName
+last_name = input("Please insert your last name 🥺")
+print("Your last name is: " + last_name.lower())
+
+# Hello @FullName
+print("Your fullname is: " + first_name + " " + last_name)
+print(("Your fullname is: " + first_name + " " + last_name).upper())
+print(("Your fullname is: " + first_name + " " + last_name).lower())
+print(("Your fullname is: " + first_name + " " + last_name).capitalize())
