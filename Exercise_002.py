1389e547b2aa01a7cf50d50590d010b981991c15
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-06-10 00:52:18 +0100

    Exercício 002

---
+++ Exercise_002.py
@@ -0,0 +1,7 @@
+print("====== DESAFIO 002 ======")
+
+day = input("Dia = ")
+mounth = input("Mês = ")
+year = input("Ano = ")
+
+print("Você nasceu no dia {} de {} de {}. Correcto?".format(day, mounth, year))
