0a1c81ae73895e78814922394361bbc15050356d
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-06-10 00:52:38 +0100

    Exercício 003

---
+++ Exercise_003.py
@@ -0,0 +1,6 @@
+print("====== DESAFIO 003 ======")
+
+first_number = int(input("Primeiro número "))
+second_number = int(input("Segundo número "))
+
+print("A soma é ", first_number + second_number)
