e356a3416ca0d6be737c9ef32e96c10c87880793
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-06-27 03:39:15 +0100

    Exercício 005

---
+++ Exercise_005.py
@@ -0,0 +1,5 @@
+print("=" * 6 + " Desafio 005 " + "=" * 6)
+
+number = int(input("Digite um número: "))
+
+print("Analisando o valor {}, seu antecessor é {} e o sucessor é {}".format(number, (number - 1), (number + 1)))
