5711c66b432906de17e7a567918e93d9bc63fca7
DorivaTech <dorivatech@gmail.com>
2021-07-02 10:08:27 +0100

    Exercício 006 - Dobro, triplo e raíz quadrada

---
+++ Exercise_006.py
@@ -0,0 +1,6 @@
+print("{:=^25}".format(" DESAFIO 006 "))
+
+number = int(input("Digite um número: "))
+print("O dobro de {1} vale {0}".format(number * 2, number))
+print("O triplo de {1} vale {0}".format(number * 3, number))
+print("A raíz quadrada de {1} é igual a {0:.3f}".format(pow(number, 1/2), number))
