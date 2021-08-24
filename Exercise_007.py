a36255e7bd692a75fe63d5f044c8c6d6a6419a08
DorivaTech <dorivatech@gmail.com>
2021-08-24 05:13:08 -0400

    Validando as notas no programa da media (permitindo apenas valores entre 0 e 20)

--- Exercise_007.py
+++ Exercise_007.py
@@ -1,7 +1,19 @@
 print("{:=^25}".format(" DESAFIO 007 "))
 print("{:=^25}".format(" Média do aluno "))
 
-number1 = float(input("Primeira nota do aluno: "))
-number2 = float(input("Segunda nota do aluno: "))
+count = 0
+
+while(True):
+	if count >= 1:
+		print("Por favor use notas no intervalo 0 - 20")
+
+	number1 = float(input("Primeira nota do aluno: "))
+	number2 = float(input("Segunda nota do aluno: "))
+
+	if number1 <= 20 and number2 <= 20:
+		break
+	
+	count += 1
+
 
 print("A média entre {} e {} é igual a {:.1f}".format(number1, number2, ((number1 + number2)/2)))
