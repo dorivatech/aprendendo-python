4af8d7ec0515beddf138674ff16a8d892ab4a5d8
DorivaTech <dorivatech@gmail.com>
2021-10-21 09:54:32 -0400

    Utilização de um expoente para representar m2 #11

--- Exercise_011.py
+++ Exercise_011.py
@@ -5,7 +5,7 @@ width = float(input("Largura da parede: "))
 height = float(input("Altura da parede: "))
 area = width * height
 
-print("Sua parede tem a dimensão de {}X{} e sua área é de {}m2.".format(width, height, area))
+print("Sua parede tem a dimensão de {}X{} e sua área é de {}m².".format(width, height, area))
 
 # Supondo que cada 2m^2 de parede precise de 1l de tinta para ser pintado
 print("Para pintar essa parede, você precisará de {}l de tinta.".format((area / 2)))
