e647ed3d318ebba78d52a0da5fc06440669c5921
DorivaTech <dorivatech@gmail.com>
2021-10-21 10:18:47 -0400

    Exercício 016 - Porção inteira de um número (com import de math)

--- Exercise_016.py
+++ Exercise_016.py
@@ -1,6 +1,8 @@
+import math
+
 print("{:=^25}".format(" DESAFIO 016 "))
 print("{:=^25}".format(" Porção inteira "), end="\n\n")
 
 number = float(input("Digite um valor numérico "))
 
-print("O valor digitado foi {} e a sua porção inteira é {:.0f}".format(number, number // 1))
+print("O valor digitado foi {} e a sua porção inteira é {:.0f}".format(number, math.trunc(number)))
