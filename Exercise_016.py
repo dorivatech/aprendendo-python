40da98b09cf0fdc221f1d332bea53279b33f30e1
DorivaTech <dorivatech@gmail.com>
2021-10-21 10:20:08 -0400

    Exercício 016 - Porção inteira de um número (com import de trunc from math)

--- Exercise_016.py
+++ Exercise_016.py
@@ -1,8 +1,8 @@
-import math
+from math import trunc
 
 print("{:=^25}".format(" DESAFIO 016 "))
 print("{:=^25}".format(" Porção inteira "), end="\n\n")
 
 number = float(input("Digite um valor numérico "))
 
-print("O valor digitado foi {} e a sua porção inteira é {:.0f}".format(number, math.trunc(number)))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(number, trunc(number)))
