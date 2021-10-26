d9747cdd1b766ea96a0296bd9d429b3d534294e7
DorivaTech <dorivatech@gmail.com>
2021-10-26 14:58:04 -0400

    Solução meio louca, para remover o espaço entre o nome digitado pelo user e a expressão '! PRAZER' #001

--- Exercise_001.py
+++ Exercise_001.py
@@ -1,4 +1,5 @@
 print("====== DESAFIO 001 ======")
 
 name = input("Qual é o seu nome? ")
-print("Olá", name, "! Prazer em te conhecer!")
+print("Olá", name, end="")
+print("! Prazer em te conhecer!")
