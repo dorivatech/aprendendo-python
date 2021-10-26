6361a5af9cc79bc277ddd1acb2ac94f942c19fa9
DorivaTech <dorivatech@gmail.com>
2021-10-26 15:35:26 -0400

    Documentando o DESAFIO 003

--- Exercise_003.py
+++ Exercise_003.py
@@ -1,3 +1,6 @@
+""" DESAFIO 003 »» Adição """
+""" Esse algoritmo recebe 2 números e depois mostra a resultante da adição entre eles """
+
 print("====== DESAFIO 003 ======")
 
 first_number = float(input("Primeiro número "))
